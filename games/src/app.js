ccd0404f991d3e355b4be6a93b01608fa162e3b4
Carol Alves <prof.carolalves90@gmail.com>
2022-06-16 17:51:22 -0300

    :bug: fix: configuração em data base

--- games/src/app.js
+++ games/src/app.js
@@ -3,12 +3,18 @@ const index = require("./routes/index")
 const gamesRoutes = require("../src/routes/gamesRoutes")
 const db = require("./config/dbConnect")
 
+db.on("error", console.log.bind(console, "Erro de conexão"))
+db.once("open", () => {
+    console.log("Conexão com o banco de dados feita com sucesso")
+})
+
 const app = express()
 
 app.use(express.json())
+
 app.use(function (request, response, next){
     response.header("Access-Control-Allow-Origin", "*")
-    res.header(
+    response.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     )
@@ -17,7 +23,5 @@ app.use(function (request, response, next){
 
 app.use("/", index)
 app.use("/games", gamesRoutes)
-
-db.connect()
 
 module.exports = app
